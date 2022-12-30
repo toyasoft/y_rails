794adaa9d7313841e71d381415b7ac5ce129018e
toyasoft <info@toyasoft.com>
2022-12-30 17:40:45 +0900

    init graphql

---
+++ db/migrate/20221230075241_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :email
+      t.integer :point
+      t.string :name
+      t.string :string
+
+      t.timestamps
+    end
+  end
+end
